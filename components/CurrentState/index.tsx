6256b17ce213619def925051b2e258c063215c51
Tom Watson <tom@tomjwatson.com>
2020-03-29 18:31:43 +0200

    improve lorem ipsum

--- components/CurrentState/index.tsx
+++ components/CurrentState/index.tsx
@@ -7,8 +7,9 @@ export default function CurrentState() {
   return (
     <Section>
       <p className={globalStyles.callout}>Current estimates suggest that we have <span className={globalStyles.red}>5 years</span> left to address climate change</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <ExampleGraph />
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
     </Section>
   )
 }
