e6d541308f2e780016762f9d785114eb77d63071
Tom Watson <tom@tomjwatson.com>
2020-05-09 17:11:12 +0200

    example production v emissions graph

---
+++ components/CurrentState/ProductionAndEmissions.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react'
+import {Doughnut} from 'react-chartjs-2'
+import classnames from 'classnames'
+import {ButtonGroup, Button, Classes} from '@blueprintjs/core'
+import styles from './ProductionAndEmissions.scss'
+
+const productionData = [4, 7, 4, 27, 24, 34]
+const emissionsData = [0.2, 0.5, 0.3, 39, 20, 40]
+
+enum TabName {
+  Production = "PRODUCTION",
+  Emissions = "EMISSIONS"
+}
+
+export default function ProductionAndEmissions() {
+  const [selectedTabId, setSelectedTabId] = React.useState<TabName>(TabName.Production)
+
+  const data = React.useMemo(() => {
+    console.log('memo', selectedTabId === TabName.Production ? [...productionData] : [...emissionsData])
+    return {
+      labels: [
+        'Nuclear',
+        'Hydro',
+        'Other (Renewables)',
+        'Coal',
+        'Gas',
+        'Oil'
+      ],
+      datasets: [{
+        data: selectedTabId === TabName.Production ? [...productionData] : [...emissionsData],
+        backgroundColor: [
+          '#43BF4D',
+          '#669EFF',
+          '#B6D94C',
+          '#63411E',
+          '#A66321',
+          '#182026'
+        ],
+      }]
+    }
+  }, [selectedTabId])
+
+  console.log(selectedTabId, data.datasets[0].data)
+
+  const productionClassNames = classnames({
+    [Classes.ACTIVE]: selectedTabId === TabName.Production
+  })
+  const emissionsClassNames = classnames({
+    [Classes.ACTIVE]: selectedTabId === TabName.Emissions
+  })
+
+  return (
+    <>
+      <div className={styles.toggleContainer}>
+        <ButtonGroup className={styles.toggle}>
+          <Button
+            className={productionClassNames}
+            onClick={() => setSelectedTabId(TabName.Production)}
+          >
+            Production
+          </Button>
+          <Button
+            className={emissionsClassNames}
+            onClick={() => setSelectedTabId(TabName.Emissions)}
+          >
+            Emissions
+          </Button>
+        </ButtonGroup>
+      </div>
+      <div style={{height: 500, marginTop: 40, marginBottom: 40}}>
+        <Doughnut data={data} />
+      </div>
+    </>
+  )
+}
--- components/CurrentState/index.tsx
+++ components/CurrentState/index.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react'
 import Section from '../Section'
 import ExampleGraph from './ExampleGraph'
+import ProductionAndEmissions from './ProductionAndEmissions'
 import globalStyles from '../../pages/Global.scss'
 
 export default function CurrentState() {
   return (
     <Section>
-      <p className={globalStyles.callout}>Current estimates suggest that we have <span className={globalStyles.red}>5 years</span> left to address climate change</p>
+      <p className={globalStyles.callout}>Based on current emissions, we are projected to lock in 2c of global warming in <span className={globalStyles.red}>5 years</span>.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <ExampleGraph />
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <p>As of 2020, X% of our energy comes from dirty, high-carbon, energy sources.</p>
+      <ProductionAndEmissions />
     </Section>
   )
 }
